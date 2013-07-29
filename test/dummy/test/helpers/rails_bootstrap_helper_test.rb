6b730c4f0dfe1da203296d762da0502869988273
Juan David Pastas Rivera <juandavid1707@gmail.com>
2013-07-28 22:15:39 -0500

    Rails 4 compatible. First try. Tests passing.

--- test/dummy/test/helpers/rails_bootstrap_helper_test.rb
+++ test/dummy/test/helpers/rails_bootstrap_helper_test.rb
@@ -27,26 +27,26 @@ class RailsBootstrapHelperTest < ActionView::TestCase
   end
 
   def test_button_link_to
-    assert_equal button_link_to("Default", "http://google.com"), '<a href="http://google.com" class="btn">Default</a>'
-    assert_equal button_link_to("Small Button", "http://google.com", size: 'small'), '<a href="http://google.com" class="btn btn-small">Small Button</a>'
-    assert_equal button_link_to("Default Success", "http://google.com", level: 'success'), '<a href="http://google.com" class="btn btn-success">Default Success</a>'
-    assert_equal button_link_to("Default Small Success", "http://google.com", size: 'small', level: 'success'), '<a href="http://google.com" class="btn btn-small btn-success">Default Small Success</a>'
-    assert_equal button_link_to("Custom Class Default Small Success", "http://google.com", class: "custom", size: 'small', level: 'success'), '<a href="http://google.com" class="btn btn-small btn-success custom">Custom Class Default Small Success</a>'
-    assert_equal button_link_to("Custom ID Default Small Success", "http://google.com", id: "custom", size: 'small', level: 'success'), '<a href="http://google.com" class="btn btn-small btn-success" id="custom">Custom ID Default Small Success</a>'
+    assert_equal button_link_to("Default", "http://google.com"), '<a class="btn" href="http://google.com">Default</a>'
+    assert_equal button_link_to("Small Button", "http://google.com", size: 'small'), '<a class="btn btn-small" href="http://google.com">Small Button</a>'
+    assert_equal button_link_to("Default Success", "http://google.com", level: 'success'), '<a class="btn btn-success" href="http://google.com">Default Success</a>'
+    assert_equal button_link_to("Default Small Success", "http://google.com", size: 'small', level: 'success'), '<a class="btn btn-small btn-success" href="http://google.com">Default Small Success</a>'
+    assert_equal button_link_to("Custom Class Default Small Success", "http://google.com", class: "custom", size: 'small', level: 'success'), '<a class="btn btn-small btn-success custom" href="http://google.com">Custom Class Default Small Success</a>'
+    assert_equal button_link_to("Custom ID Default Small Success", "http://google.com", id: "custom", size: 'small', level: 'success'), '<a class="btn btn-small btn-success" href="http://google.com" id="custom">Custom ID Default Small Success</a>'
   end
 
   def test_icon_button_link_to
-    assert_equal icon_button_link_to("Default", "http://google.com", icon: 'plus'), '<a href="http://google.com" class="btn"><i class="icon-plus"></i> Default</a>'
-    assert_equal icon_button_link_to("Small Button", "http://google.com", size: 'small', icon: 'plus'), '<a href="http://google.com" class="btn btn-small"><i class="icon-plus"></i> Small Button</a>'
-    assert_equal icon_button_link_to("Default Success", "http://google.com", level: 'success', icon: 'plus'), '<a href="http://google.com" class="btn btn-success"><i class="icon-plus"></i> Default Success</a>'
-    assert_equal icon_button_link_to("Default Small Success", "http://google.com", size: 'small', level: 'success', icon: 'plus'), '<a href="http://google.com" class="btn btn-small btn-success"><i class="icon-plus"></i> Default Small Success</a>'
-    assert_equal icon_button_link_to("Custom Class Default Small Success", "http://google.com", class: "custom", size: 'small', level: 'success', icon: 'plus'), '<a href="http://google.com" class="btn btn-small btn-success custom"><i class="icon-plus"></i> Custom Class Default Small Success</a>'
-    assert_equal icon_button_link_to("Custom ID Default Small Success", "http://google.com", id: "custom", size: 'small', level: 'success', icon: 'plus'), '<a href="http://google.com" class="btn btn-small btn-success" id="custom"><i class="icon-plus"></i> Custom ID Default Small Success</a>'
+    assert_equal icon_button_link_to("Default", "http://google.com", icon: 'plus'), '<a class="btn" href="http://google.com"><i class="icon-plus"></i> Default</a>'
+    assert_equal icon_button_link_to("Small Button", "http://google.com", size: 'small', icon: 'plus'), '<a class="btn btn-small" href="http://google.com"><i class="icon-plus"></i> Small Button</a>'
+    assert_equal icon_button_link_to("Default Success", "http://google.com", level: 'success', icon: 'plus'), '<a class="btn btn-success" href="http://google.com"><i class="icon-plus"></i> Default Success</a>'
+    assert_equal icon_button_link_to("Default Small Success", "http://google.com", size: 'small', level: 'success', icon: 'plus'), '<a class="btn btn-small btn-success" href="http://google.com"><i class="icon-plus"></i> Default Small Success</a>'
+    assert_equal icon_button_link_to("Custom Class Default Small Success", "http://google.com", class: "custom", size: 'small', level: 'success', icon: 'plus'), '<a class="btn btn-small btn-success custom" href="http://google.com"><i class="icon-plus"></i> Custom Class Default Small Success</a>'
+    assert_equal icon_button_link_to("Custom ID Default Small Success", "http://google.com", id: "custom", size: 'small', level: 'success', icon: 'plus'), '<a class="btn btn-small btn-success" href="http://google.com" id="custom"><i class="icon-plus"></i> Custom ID Default Small Success</a>'
   end
 
   def test_percent_input
-    assert_match '<div class="input-prepend"><span class="add-on">%</span> <input class="string required" id="foo_name" name="foo[name]" size="50" type="text" /></div>', simple_form_for(:foo, url: '') {|f| percent_input(f, :name, "Name") }
-    assert_match '<div class="input-append"><input class="string required" id="foo_name" name="foo[name]" size="50" type="text" /> <span class="add-on">%</span></div>', simple_form_for(:foo, url: '') {|f| percent_input(f, :name, "Name", append: true) }
+    assert_match '<div class="input-prepend"><span class="add-on">%</span> <input class="string required" id="foo_name" name="foo[name]" type="text" /></div>', simple_form_for(:foo, url: '') {|f| percent_input(f, :name, "Name") }
+    assert_match '<div class="input-append"><input class="string required" id="foo_name" name="foo[name]" type="text" /> <span class="add-on">%</span></div>', simple_form_for(:foo, url: '') {|f| percent_input(f, :name, "Name", append: true) }
   end
   
 end
